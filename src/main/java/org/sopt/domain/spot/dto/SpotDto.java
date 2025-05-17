1b65bb139fab26018275c98a356c5f8b26c6ef4d
hyeumm <20221159@sungshin.ac.kr>
2025-05-18 06:33:19 +0900

    chore: spot Dto 설명 추가

--- src/main/java/org/sopt/domain/spot/dto/SpotDto.java
+++ src/main/java/org/sopt/domain/spot/dto/SpotDto.java
@@ -1,18 +1,21 @@
 package org.sopt.domain.spot.dto;
 
+import org.sopt.domain.spot.domain.Spot;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.sopt.domain.spot.domain.Spot;
 
 public record SpotDto(
         String spotName,
         String address,
+        String description,
         List<String> imageUrls
 ) {
     public static SpotDto from(Spot spot) {
         return new SpotDto(
                 spot.getSpotName(),
                 spot.getAddress(),
+                spot.getDescription(),
                 new ArrayList<>(spot.getImageUrls()) // ← 여기서 미리 초기화해야 LAZY 문제 없음
         );
     }
