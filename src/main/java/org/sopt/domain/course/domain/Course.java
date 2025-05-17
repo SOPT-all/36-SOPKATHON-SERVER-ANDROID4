604e516c17c769bc2ee4ce5fbd9e19d27891bef2
hyeumm <20221159@sungshin.ac.kr>
2025-05-18 01:14:36 +0900

    feature: Course 엔티티 생성

---
+++ src/main/java/org/sopt/domain/course/domain/Course.java
@@ -0,0 +1,28 @@
+package org.sopt.domain.course.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.sopt.domain.user.domain.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long courseId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String courseTitle;
+    private String description;
+    private LocalDate recordDate;
+}
