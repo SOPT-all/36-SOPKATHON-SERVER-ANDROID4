035cbcad2fa906990ac09fc623679f9237cab910
hyeumm <20221159@sungshin.ac.kr>
2025-05-18 06:14:14 +0900

    chore: 마이페이지 사진 조회 추가

--- src/main/java/org/sopt/domain/course/dto/CourseSummaryDto.java
+++ src/main/java/org/sopt/domain/course/dto/CourseSummaryDto.java
@@ -1,15 +1,27 @@
 package org.sopt.domain.course.dto;
 
 import org.sopt.domain.course.domain.Course;
+import org.sopt.domain.spot.domain.Spot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public record CourseSummaryDto(
         String courseTitle,
-        String description
+        String description,
+        List<String> imageUrls
 ) {
     public static CourseSummaryDto from(Course course) {
+        List<Spot> spots = course.getSpots();
+
+        List<String> imageUrls = (!spots.isEmpty() && spots.get(0).getImageUrls() != null)
+                ? new ArrayList<>(spots.get(0).getImageUrls().subList(0, 2))
+                : List.of();
+
         return new CourseSummaryDto(
                 course.getCourseTitle(),
-                course.getDescription()
+                course.getDescription(),
+                imageUrls
         );
     }
 }
